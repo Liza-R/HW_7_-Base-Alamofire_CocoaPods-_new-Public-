c01cca2eda2251414cc69c0888133567873026eb
LioRin <ex@mail.com>
2020-12-03 11:49:47 +0300

    Initial Commit

---
+++ HW_7_/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW_7_
+//
+//  Created by Lio Rin on 03/12/2020.
+//  Copyright © 2020 Lio Rin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW_7_Tests/HW_7_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HW_7_Tests.swift
+//  HW_7_Tests
+//
+//  Created by Lio Rin on 03/12/2020.
+//  Copyright © 2020 Lio Rin. All rights reserved.
+//
+
+import XCTest
+@testable import HW_7_
+
+class HW_7_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
